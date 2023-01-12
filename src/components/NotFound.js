4db8ce671c3756f13feba72b9e49fb278b03108e
Facundo <facugastonbarral@gmail.com>
2023-01-11 23:44:41 -0300

    Created NotFound component

---
+++ src/components/NotFound.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Inicio(props) {
+  return (
+    <main>
+        <div className="text-center inicio-main">
+            <h1>Esta página no existe</h1>
+        </div>
+    </main>
+  );
+}
