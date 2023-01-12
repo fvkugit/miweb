0be6905162461050a420233febfcf45a9c84fb68
Facundo <facugastonbarral@gmail.com>
2023-01-11 23:52:18 -0300

    Created Footer

---
+++ src/components/Footer.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function Footer(props) {
+  return (
+    <>
+      <footer>
+        <div className="footer text-center">
+          <p>Hecho con ❤️ y Google.</p>
+        </div>
+      </footer>
+    </>
+  );
+}
