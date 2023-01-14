a10cacc5580863c5800d88bff713f85bdc2dbf40
Facundo <facugastonbarral@gmail.com>
2023-01-14 01:12:01 -0300

    Added routes with pages

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import Inicio from "./components/Inicio";
+// import Proyectos from "./components/Proyectos";
+import NotFound from "./components/NotFound";
+import Base from "./components/Base";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Routes>
+        <Route path="/" element={<Base />}>
+          <Route index element={<Inicio />} />
+          {/* <Route path="proyectos" element={<Proyectos />} /> */}
+          <Route path="*" element={<NotFound />} />
+        </Route>
+      </Routes>
+    </>
   );
 }
 
