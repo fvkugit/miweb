34953b8efcdb78da4751dcf95874779d25822471
Facundo <facugastonbarral@gmail.com>
2023-01-16 22:38:56 -0300

    Add link on contact buttons

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -10,9 +10,9 @@ export default function Inicio(props) {
           <h1 className="shadow-1">Contacto</h1>
           
           <div className="contact-buttons">
-            <a className="shadow-1" href="/"><SiGmail className="k-icon contact-icon"/></a>
-            <a className="shadow-1" href="/"><SiLinkedin className="k-icon contact-icon"/></a>
-            <a className="shadow-1" href="/"><SiGithub className="k-icon contact-icon"/></a>
+            <a className="shadow-1" href="mailto:facugastonbarral@gmail.com"><SiGmail className="k-icon contact-icon"/></a>
+            <a className="shadow-1" href="https://www.linkedin.com/in/facundobarral/" target="_blank" rel="noreferrer"><SiLinkedin className="k-icon contact-icon"/></a>
+            <a className="shadow-1" href="https://github.com/fvkugit" target="_blank" rel="noreferrer"><SiGithub className="k-icon contact-icon"/></a>
           </div>
         </div>
       </div>
